f82ef325b5972371c4e623509dabe6fdcbfc3f18
zhumingjie1996 <1005614322@qq.com>
2026-06-02 23:34:17 +0800

    Initial Commit

---
+++ hold-it-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hold-it-ios
+//
+//  Created by zhumingjie on 2026/6/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hold-it-ios/hold_it_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  hold_it_iosApp.swift
+//  hold-it-ios
+//
+//  Created by zhumingjie on 2026/6/2.
+//
+
+import SwiftUI
+
+@main
+struct hold_it_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
